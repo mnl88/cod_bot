0b3a8dd549ca00e2a8196e15f979124a6f2b5031
Nikita_MSI_PX60 <mnl88@yandex.ru>
2020-10-27 16:57:32 +0300

    Убрал из докеригнора config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+# Telegram settings
+API_TOKEN = '1286213260:AAGtDDTmEup0E7CW5O9jAXfekDLxIztEL9Y'  # bot token from @BotFather
+ADMIN_ID: int = 202181776
+CHAT_ID: int = 1
+CHAT_TEST_ID: int = 0
+
+# Database settings
+DATABASE_USER = 'Nikita'
+DATABASE_PASSWORD = '9492088'
+DATABASE_HOST = 'vps-99553d11.vps.ovh.net'
+DATABASE_PORT = '5432'
+DATABASE_NAME = 'call_of_duty'
