23c844097cd77ce733e2667e8d00a9c413a66a69
Nikita_MSI_PX60 <mnl88@yandex.ru>
2020-12-06 16:40:07 +0300

    HEADRES update

--- cod_stats_parser.py
+++ cod_stats_parser.py
@@ -20,10 +20,8 @@ URL_MP_FINISH = '/mp'
 URL_WZ_START = 'warzone/profile/atvi/'
 URL_WZ_FINISH = '/overview'
 HEADERS = {
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,'
-              'image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
-                  '(KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36'
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'
 }
 
 
